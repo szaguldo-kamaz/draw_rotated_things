f000159ebec2c7653e289541000036ad1ea452b7
szaguldo-kamaz <github@kamm.hu>
2021-08-07 01:05:48 +0900

    make draw_rotated_rectangle a wrapper around draw_rotated_polygon

--- draw_rotated_things.py
+++ draw_rotated_things.py
@@ -38,33 +38,19 @@ def draw_rotated_polygon(graph, polycoords, polycenter, angle, outlinecolor, rot
 
 def draw_rotated_rectangle(graph, rectcoord, angle, outlinecolor, rotcentangle=0, rotcentdist=0, fill=True, fillcolor=None, linewidth=1):
 
-    coord=( rectcoord[0], (rectcoord[1][0],rectcoord[0][1]), rectcoord[1], (rectcoord[0][0],rectcoord[1][1]) );
-    coord_center=( int((rectcoord[0][0]+rectcoord[1][0])/2), int((rectcoord[0][1]+rectcoord[1][1])/2) );
-    cosangle=math.cos(angle);
-    sinangle=math.sin(angle);
-
-    cosangle_cd=math.cos(rotcentangle);
-    sinangle_cd=math.sin(rotcentangle);
-    coord_cd=(coord_center[0],coord_center[1]-rotcentdist);
-
-    coord_rot=[];
-    for c in range(0,4):
-        temp=[ (coord[c][0]-coord_center[0])*cosangle - (coord[c][1]-coord_center[1])*sinangle + coord_center[0],
-               (coord[c][0]-coord_center[0])*sinangle + (coord[c][1]-coord_center[1])*cosangle + coord_center[1] ];
-        coord_rot.append(
-              [ (temp[0]-coord_cd[0])*cosangle_cd - (temp[1]-coord_cd[1])*sinangle_cd + coord_center[0],
-                (temp[0]-coord_cd[0])*sinangle_cd + (temp[1]-coord_cd[1])*cosangle_cd + coord_center[1] ] );
-
-    if fill:
-        if fillcolor==None:
-            fillcolor=outlinecolor;
-        return graph.draw_polygon(coord_rot, line_color=outlinecolor, fill_color=fillcolor);
-    else:
-        coord_rot.append(coord_rot[0]);
-        return graph.draw_lines(coord_rot, width=linewidth, color=outlinecolor);
+    polycoords=( rectcoord[0], (rectcoord[1][0],rectcoord[0][1]), rectcoord[1], (rectcoord[0][0],rectcoord[1][1]) );
+    polycenter=( int((rectcoord[0][0]+rectcoord[1][0])/2), int((rectcoord[0][1]+rectcoord[1][1])/2) );
+    return draw_rotated_polygon(graph, polycoords, polycenter, angle, outlinecolor, rotcentangle=rotcentangle, rotcentdist=rotcentdist, fill=fill, fillcolor=fillcolor, linewidth=linewidth);
 
 
 def draw_rotated_line(graph, coord, angle, color, rotcentangle=0, rotcentdist=0, linewidth=1):
+
+# this could also be just a draw_rotate_polygon wrapper...
+#    polycoords=coord;
+#    polycenter=( int((coord[0][0]+coord[1][0])/2), int((coord[0][1]+coord[1][1])/2) );
+#    return draw_rotated_polygon(graph, polycoords, polycenter, angle, color, rotcentangle=rotcentangle, rotcentdist=rotcentdist, fill=False, linewidth=linewidth);
+#
+# ... but this one is probably cheaper
     coord_center=( int((coord[0][0]+coord[1][0])/2), int((coord[0][1]+coord[1][1])/2) );
     cosangle=math.cos(angle);
     sinangle=math.sin(angle);
