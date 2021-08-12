c6f3569bb426fa9572b08917ab8ab4525abf94da
szaguldo-kamaz <github@kamm.hu>
2021-08-12 21:42:05 +0900

    add some comments

--- Demo_draw_radar_screen.py
+++ Demo_draw_radar_screen.py
@@ -30,25 +30,32 @@ graph = window['-GRAPH-']
 graph.draw_text("száguldó", [graphsize[0]-40, graphsize[1]-22],color='#00A000', font="Verdana 7", angle=14);
 graph.draw_text("kamaz", [graphsize[0]-22, graphsize[1]-16],color='#00A000', font="Verdana 7", angle=14);
 
+# to make a smoother transition from the background color to the color of the circles there will be three layers
+# first layer of circles
 for rcn in range(50,300,50):
     graph.draw_circle( [graphsize[0]/2,graphsize[1]/2], rcn, line_color='#005000', line_width=6);
+# first layer of the cross
 graph.draw_line( [graphsize[0]/2,graphsize[1]-20],  [graphsize[0]/2,20],  color='#005000', width=6);
 graph.draw_line( [graphsize[0]-120,graphsize[1]/2], [120,graphsize[1]/2], color='#005000', width=6);
 
+# second layer of circles
 for rcn in range(50,300,50):
     graph.draw_circle( [graphsize[0]/2,graphsize[1]/2], rcn, line_color='#007000', line_width=4);
+# second layer of the cross
 graph.draw_line( [graphsize[0]/2,graphsize[1]-22],  [graphsize[0]/2,22],  color='#007000', width=4);
 graph.draw_line( [graphsize[0]-122,graphsize[1]/2], [122,graphsize[1]/2], color='#007000', width=4);
 
+# third layer of circles
 topcircles={};
 for rcn in range(50,300,50):
     topcircles[rcn]=graph.draw_circle( [graphsize[0]/2,graphsize[1]/2], rcn, line_color='#009000', line_width=2);
-
+# third layer of the cross
 topcross=[ graph.draw_line( [graphsize[0]/2,graphsize[1]-24],  [graphsize[0]/2,24],  color='#009000', width=2),
            graph.draw_line( [graphsize[0]-124,graphsize[1]/2], [124,graphsize[1]/2], color='#009000', width=2) ];
 
-drl={};
-ran=0;
+drl={}; # for storing the line objects
+ran=0; # angle of the current line
+# colors of the "fading" lines
 rlcolors=[ '#004000', '#004800', '#005000', '#005800', '#006000', '#006800', '#006B00', '#007000', '#007400',
            '#007800', '#007B00', '#008000', '#008400', '#008800', '#008B00', '#009000', '#00A000', '#00FF00' ];
 
@@ -61,6 +68,7 @@ while True:
 
     ran=ran+0.05;
 
+    # bring forward the third layer, so the newly drawn lines will be under it
     for rcn in range(50,300,50):
         graph.bring_figure_to_front(topcircles[rcn]);
     graph.bring_figure_to_front(topcross[0]);
